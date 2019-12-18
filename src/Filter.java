180272444331a9eb5fd9fc2e78f5ece2b9747bab
elshan777 <elshen.qedimov2000@gmail.com>
2019-12-18 13:02:41 +0400

    file uploader added, new image added, resizing does not work

--- src/Filter.java
+++ src/Filter.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -9,18 +10,30 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JToggleButton;
-
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
 public class Filter extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
 	 */
+	File f = new File("old.jpg");
+	BufferedImage image = ImageIO.read(f);
+	int count =1;
+	JLabel lblNewLabel;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -47,15 +60,22 @@ public class Filter extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
-		File f = new File("old.jpg");
-		BufferedImage image = ImageIO.read(f);
-
-		JLabel lblNewLabel = new JLabel(new ImageIcon(image));
-		// lblNewLabel.setIcon(icon);
-		lblNewLabel.setBounds(12, 12, 500, 500);
+		
+		JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+		int returnValue = jfc.showOpenDialog(null);
+		if (returnValue == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = jfc.getSelectedFile();
+			System.out.println(selectedFile.getAbsolutePath());
+			f = new File(selectedFile.getAbsolutePath());
+			image = ImageIO.read(f);
+		}
+		
+		lblNewLabel = new JLabel(new ImageIcon(image));
+		lblNewLabel.setBounds(50, 50, 500, 500);
 		contentPane.add(lblNewLabel);
-
+		
+		
+//////////////////////////////------The Brightest Part------///////////////////////////////////////
 		JButton btnRed = new JButton("Bright");
 		btnRed.setBounds(580, 103, 156, 25);
 		contentPane.add(btnRed);
@@ -79,29 +99,19 @@ public class Filter extends JFrame {
 
 					}
 				}
-				System.out.println(new Color(10, 20, 50).getRGB());
-				// BufferedImage imagez = new BufferedImage(imagex.getWidth(),
-				// imagex.getHeight(), BufferedImage.TYPE_INT_RGB);
-				// lblNewLabel.paint(imagex.getGraphics());
 				lblNewLabel.setIcon(new ImageIcon(image));
-				// lblNewLabel.setBounds(12, 12, 500, 500);
-				// JOptionPane.showMessageDialog(null, new JLabel(new ImageIcon(imagex)));
-//				try {
-//					ImageIO.write(image, "png", new File("imagex.png"));
-//				} catch (IOException e1) {
-//					e1.printStackTrace();
-//				}
 
 			}
 		});
 
-		JButton btnOne = new JButton("Black and White");
+//////////////////////////////------Different Part------///////////////////////////////////////
+		JButton btnOne = new JButton("Different");
 		btnOne.setBounds(580, 140, 154, 25);
 		contentPane.add(btnOne);
 		btnOne.addActionListener(new ActionListener() {
-
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				int z = 5;
 				for (int xx = 0; xx < image.getWidth() - 2; xx += 2) {
 					for (int yy = 0; yy < image.getHeight() - 2; yy += 2) {
 						int pixel = image.getRGB(xx, yy);
@@ -109,21 +119,24 @@ public class Filter extends JFrame {
 						int red = (pixel >> 16) & 0x0ff;
 						int green = (pixel >> 8) & 0x0ff;
 						int blue = (pixel) & 0x0ff;
-						// System.out.println(red+" "+ green+" "+blue);
-						image.setRGB(xx + 1, yy, new Color(red, green, blue).getRGB());
-						image.setRGB(xx, yy + 1, new Color(red, green, blue).getRGB());
-						image.setRGB(xx + 1, yy + 1, new Color(red, green, blue).getRGB());
+						
+						
+						int nred = (red - red/z) <= 0 ? 0 : (red - red/z);
+						int ngreen = (green + green/z) > 255 ? 255 : (green + green/z);
+						int nblue = (blue - blue/z) <= 0 ? 0 : (blue - blue/z);
+						image.setRGB(xx, yy, new Color(nred, ngreen, nblue).getRGB());
 					}
 
 				}
 				lblNewLabel.setIcon(new ImageIcon(image));
 			}
 		});
-
+		
+		
+//////////////////////////////------Cold Part------///////////////////////////////////////
 		JButton btnTwo = new JButton("Cold");
 		btnTwo.setBounds(580, 177, 158, 25);
 		contentPane.add(btnTwo);
-
 		btnTwo.addActionListener(new ActionListener() {
 
 			@Override
@@ -137,7 +150,7 @@ public class Filter extends JFrame {
 						int blue = (pixel) & 0x0ff;
 
 						if (blue < 250) {
-							image.setRGB(xx, yy, new Color(red, green, blue+50>255?255:blue+50).getRGB());
+							image.setRGB(xx, yy, new Color(red-20<0?0:red-20, green, blue+40>255?255:blue+40).getRGB());
 
 						}
 
@@ -149,22 +162,15 @@ public class Filter extends JFrame {
 			}
 		});
 
-		JButton btnSave = new JButton("Save");
-		btnSave.setBounds(624, 453, 114, 25);
-		contentPane.add(btnSave);
+	
 
-		JToggleButton tglbtnMonochrome = new JToggleButton("Monochrome");
-		tglbtnMonochrome.setBounds(580, 209, 162, 25);
-		contentPane.add(tglbtnMonochrome);
+		
 
-		JButton redbt = new JButton("Hot");
-		redbt.setBounds(580, 66, 156, 25);
-		contentPane.add(redbt);
+//////////////////////////////------Coolest Part------///////////////////////////////////////
 
 		JButton outbtn = new JButton("Cool");
 		outbtn.setBounds(576, 242, 162, 25);
 		contentPane.add(outbtn);
-
 		outbtn.addActionListener(new ActionListener() {
 
 			@Override
@@ -176,11 +182,12 @@ public class Filter extends JFrame {
 						int red = (pixel >> 16) & 0x0ff;
 						int green = (pixel >> 8) & 0x0ff;
 						int blue = (pixel) & 0x0ff;
+						
+						int nred = (red >= 130 && red<245) ? red+10 : red;
+						int ngreen = (green<20) ? 0 : (green-20);
+						int nblue = (blue >= 120 && blue<245)? blue+10 : blue;
 
-						image.setRGB(xx, yy, new Color(green, blue, red).getRGB());
-						image.setRGB(xx + 1, yy + 1, new Color(green, blue, red).getRGB());
-						image.setRGB(xx + 2, yy + 2, new Color(green, blue, red).getRGB());
-						image.setRGB(xx + 3, yy + 3, new Color(green, blue, red).getRGB());
+						image.setRGB(xx, yy, new Color(nred, ngreen, nblue).getRGB());
 
 					}
 
@@ -190,6 +197,11 @@ public class Filter extends JFrame {
 			}
 		});
 
+		
+//////////////////////////////------Red Part------///////////////////////////////////////
+		JButton redbt = new JButton("Hot");
+		redbt.setBounds(580, 66, 156, 25);
+		contentPane.add(redbt);
 		redbt.addActionListener(new ActionListener() {
 
 			@Override
@@ -212,6 +224,12 @@ public class Filter extends JFrame {
 			}
 		});
 
+		
+//////////////////////////////------Monochrome Part------///////////////////////////////////////
+
+		JToggleButton tglbtnMonochrome = new JToggleButton("Monochrome");
+		tglbtnMonochrome.setBounds(580, 209, 162, 25);
+		contentPane.add(tglbtnMonochrome);
 		tglbtnMonochrome.addActionListener(new ActionListener() {
 
 			@Override
@@ -219,8 +237,6 @@ public class Filter extends JFrame {
 				JToggleButton tBtn = (JToggleButton) e.getSource();
 				BufferedImage imagez = new BufferedImage(image.getWidth(), image.getHeight(),
 						BufferedImage.TYPE_INT_RGB);
-				// JOptionPane.showMessageDialog(null, new JLabel(new ImageIcon(imagez)));
-				// lblNewLabel.paint(imagez.getGraphics());
 				if (tBtn.isSelected()) {
 					for (int xx = 0; xx < image.getWidth(); xx++) {
 						for (int yy = 0; yy < image.getHeight(); yy++) {
@@ -245,15 +261,79 @@ public class Filter extends JFrame {
 				}
 			}
 		});
+		
+		
+		
+//////////////////////////////------Reset Part------///////////////////////////////////////
 
+		JButton btnReset = new JButton("Reset");
+		btnReset.setBounds(622, 418, 114, 25);
+		contentPane.add(btnReset);
+		btnReset.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				try {
+					image = ImageIO.read(f);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				};
+				lblNewLabel.setIcon(new ImageIcon(image));
+			}
+		});
+		
+		
+		
+		
+		
+//////////////////////////////------New Image Part------///////////////////////////////////////
+
+		JButton btnNewImage = new JButton("New Image");
+		btnNewImage.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+				int returnValue = jfc.showOpenDialog(null);
+				if (returnValue == JFileChooser.APPROVE_OPTION) {
+					File selectedFile = jfc.getSelectedFile();
+					System.out.println(selectedFile.getAbsolutePath());
+					f = new File(selectedFile.getAbsolutePath());
+					try {
+						image = ImageIO.read(f);
+						JOptionPane.showMessageDialog(null, new JLabel(new ImageIcon(image)));
+						lblNewLabel.setIcon(new ImageIcon(image));
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				
+				
+			}
+		});
+
+		btnNewImage.setBounds(622, 484, 114, 25);
+		contentPane.add(btnNewImage);
+		
+		//////////////////////////////------Save Part------///////////////////////////////////////
+		JButton btnSave = new JButton("Save");
+		btnSave.setBounds(624, 453, 114, 25);
+		contentPane.add(btnSave);
 		btnSave.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				BufferedImage new_image = new BufferedImage(lblNewLabel.getIcon().getIconWidth(),
-						lblNewLabel.getIcon().getIconHeight(), BufferedImage.TYPE_INT_RGB);
+				BufferedImage new_image = new BufferedImage(lblNewLabel.getWidth(),
+				lblNewLabel.getHeight(), BufferedImage.TYPE_INT_RGB);
+				Graphics2D g2d = new_image.createGraphics();
+				lblNewLabel.printAll(g2d);
+				g2d.dispose();
 				try {
-					ImageIO.write(new_image, "png", new File("new_image.png"));
+					ImageIO.write(image, "png", new File("new_image"+count+".png"));
+					count++;
+					JOptionPane.showMessageDialog(null, "Your image has been saved succefully."
+							+ " You can reset and save new image as well");
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
